e9a312d50ba97fdc8e8a88e91f37e9f881ff8981
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-12-22 13:40:40 +0100

    Set base

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -17,6 +17,8 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
 
+  base: '/notification-documentation/',
+
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
