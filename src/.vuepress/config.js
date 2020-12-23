0373e14fe5e595724be765e39be95a06b9cad3f2
Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
2020-12-23 13:09:35 -0500

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -121,36 +121,4 @@ module.exports = {
     ],
     '@vuepress/back-to-top',
   ]
-module.exports = {
-  plugins: [
-    // you can use this plugin multiple times
-    [
-      'vuepress-plugin-container',
-      {
-        type: 'right',
-        defaultTitle: '',
-      },
-    ],
-    [
-      'vuepress-plugin-container',
-      {
-        type: 'theorem',
-        before: info => `<div class="theorem"><p class="title">${info}</p>`,
-        after: '</div>',
-      },
-    ],
-
-    // this is how VuePress Default Theme use this plugin
-    [
-      'vuepress-plugin-container',
-      {
-        type: 'tip',
-        defaultTitle: {
-          '/': 'TIP',
-          '/zh/': '提示',
-        },
-      },
-    ],
-  ],
-}
 }
