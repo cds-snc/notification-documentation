e843ea47bedb4b59f467b34d865277409ffce6ef
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-01-13 10:01:21 -0500

    Allow indexing by search engines

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,7 +10,6 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'robots', content: 'noindex' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
