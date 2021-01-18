9d3ed9b60b200edea59c33bee480189496af5dd6
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-01-18 17:51:19 -0500

    feat: back to Notify link in sidebar (#5)

---
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,37 @@
+<template>
+  <ParentLayout>
+    <template #sidebar-top>
+      <div class="back-to-notify">
+        <ParentNavLink :item='{"link": backToLink, "text": backToText}' v-if="backToText" />
+      </div>
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+import ParentNavLink from '@parent-theme/components/NavLink.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    ParentLayout,
+    ParentNavLink,
+  },
+  computed: {
+    backToLink() {
+      return this.$themeLocaleConfig.backToNotifyLink
+    },
+    backToText() {
+      return this.$themeLocaleConfig.backToNotifyText
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.back-to-notify
+  padding 1em 0 1em 1.5em
+  border-bottom 1px solid $borderColor
+  a
+    color $textColor
+</style>
