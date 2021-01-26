62139cca3ea21db48c4b789228c69634b1d5c7ab
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-01-26 17:27:20 -0500

    Redirect based on browser lang

---
+++ src/.vuepress/theme/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  mounted() {
+    const browserLocales = navigator.languages === undefined ? [navigator.language] : navigator.languages;
+    const lang = browserLocales[0].substring(0, 2);
+
+    if (lang === 'fr') {
+      window.location.replace(window.location.href + 'fr/');
+    } else {
+      window.location.replace(window.location.href + 'en/');
+    }
+  }
+}
+</script>
--- src/index.md
+++ src/index.md
@@ -1,14 +1,3 @@
 ---
 home: true
-heroImage: https://assets.notification.canada.ca/static/mac-phone.svg
-heroText: GC Notify | GC Notification
-tagline: null
-actionText: Documentation →
-actionLink: /en/
 ---
-
-<div style="text-align: center">
-    <a href="https://notification.canada.ca?lang=en">Back to GC Notify</a>
-    |
-    <a href="https://notification.canada.ca?lang=fr">Retour à GC Notification</a>
-</div>
