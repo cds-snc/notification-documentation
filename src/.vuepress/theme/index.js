6d0336c35e1407155390692973a7ba835a4a5228
Stephen McMurtry <smcmurtry@gmail.com>
2025-09-17 11:56:00 -0400

    implement a variation of the vuepress searchbox that includes type=search (#206)

--- src/.vuepress/theme/index.js
+++ src/.vuepress/theme/index.js
@@ -1,3 +1,8 @@
+const path = require('path')
+
 module.exports = {
-  extend: '@vuepress/theme-default'
+  extend: '@vuepress/theme-default',
+  alias: {
+    '@SearchBox': path.resolve(__dirname, 'components/SearchBox.vue'),
+  }
 }
